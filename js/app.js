c1b13b4938d1bf40400b44c155d49d982689ae37
Alexurueta15 <66281082+Alexurueta15@users.noreply.github.com>
2021-10-14 12:53:22 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let principal = $('#principal');
+let notice = $('#notice');
+let btnRegresar = $('.btn-regresar')
+
+$('.btn-seguir').on('click', function (e){
+    e.preventDefault();
+    console.log("btn-seguir touched");
+    principal.fadeOut(function (){
+        btnRegresar.fadeIn();
+        notice.slideDown(1000);
+    });
+});
+btnRegresar.on('click', function (e){
+    e.preventDefault();
+    console.log("btn-seguir touched");
+    notice.fadeOut(function (){
+        principal.slideDown(1000);
+    });
+})
+
+const swDirector =
+    window.location.href.includes("localhost") ? "/sw.js" : "/Alexurueta15/U2-T1-URUETAOSCAR/sw.js";
+
+if (navigator.serviceWorker){
+    navigator.serviceWorker.register(swDirector);
+}
+
---
+++ sw.js
@@ -0,0 +1,72 @@
+console.log("SW: hola desde sw");
+const CACHE_STATIC_NAME = 'static-cache';
+const CACHE_DYNAMIC_NAME = 'dynamic-cache';
+const CACHE_INMUTABLE_NAME = 'inmutable-cache';
+
+function cleanCache(cacheName, sizeItems) {
+    caches.open(cacheName)
+        .then(cache => {
+            cache.keys().then(keys => {
+                console.log(keys);
+                if (keys.length > sizeItems) {
+                    cache.delete(keys[0]).then(() => cleanCache(cacheName, sizeItems));
+                }
+            });
+        });
+}
+
+
+self.addEventListener('install', evt => {
+    console.log("nuevo sw");
+
+    //crear cache y almacenar appshell
+    const promiseCache = caches.open(CACHE_STATIC_NAME)
+        .then(cache => {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'img/p1.png',
+                'img/p2.png',
+                'img/p3.png',
+                'img/p4.png',
+                'js/app.js'
+            ]);
+        });
+
+    const inmutableCache = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js',
+                'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
+                'https://code.jquery.com/jquery-3.5.1.min.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+            ]);
+        });
+
+    evt.waitUntil(Promise.all([promiseCache, inmutableCache]));
+});
+
+self.addEventListener('fetch', evt => {
+    // primero busca en cache, en caso de non encontrar va a la red.
+    const respuestaCache = caches.match(evt.request)
+        .then(value => {
+            //si la request existe en cache
+            if (value) {
+                //respondemos con cache
+                return value;
+            } else {
+                //vas a la red
+                console.log("no está en caché", evt.request.url)
+                return fetch(evt.request).then(resp => {
+                    //guardo la respuesta en cache
+                    caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+                        cache.put(evt.request, resp).then(() => cleanCache(CACHE_DYNAMIC_NAME, 5));
+                    });
+                    //retorno la respuesta
+                    return resp.clone();
+                });
+            }
+        });
+
+    evt.respondWith(respuestaCache);
+});
